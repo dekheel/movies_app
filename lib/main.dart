7dc11897b33055822b9ac2d25af7edb05c7dc0c0
dekheel <adekheel@gmail.com>
2024-03-14 23:46:29 +0200

    project architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/Home_screen/home_screen.dart';
 import 'package:movies_app/My_theme/my_theme.dart';
-import 'package:movies_app/home_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
