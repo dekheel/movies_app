1b783626bd8d4e3211429efecbedc357d5c217ed
dekheel <adekheel@gmail.com>
2024-03-17 00:55:49 +0200

    finish search

--- lib/My_theme/my_theme.dart
+++ lib/My_theme/my_theme.dart
@@ -6,11 +6,12 @@ class MyTheme {
   static Color blackColor = const Color(0xff121312);
   static Color greyColor = const Color(0xff282A28);
   static Color greyColor2 = const Color(0xff707070);
-
+  static Color greyColor3 = const Color(0xff514F4F);
   static Color whiteColor = const Color(0xffffffff);
   static Color yellowColor = const Color(0xffFFBB3B);
   static Color lightGreyColor = const Color(0xffC6C6C6);
   static Color redColor = const Color(0xffc7024c);
+  static Color cyanColor = const Color(0xff48CFAD);
 
   static Color bottomNavigationBackgroundColor = const Color(0xff1A1A1A);
 
---
+++ lib/screens/home_screen/Search_Widget/search_movie_item.dart
@@ -0,0 +1,133 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:movies_app/Methods/methods.dart';
+import 'package:movies_app/My_theme/my_theme.dart';
+import 'package:movies_app/api/api_constatnts.dart';
+import 'package:movies_app/api/api_manager.dart';
+import 'package:movies_app/models/general_response.dart';
+import 'package:movies_app/models/movie_detail_response.dart';
+import 'package:movies_app/screens/details_screen/detail_movie_screen.dart';
+
+class SearchMovieItem extends StatefulWidget {
+  final Movie? movie;
+
+  const SearchMovieItem({required this.movie, super.key});
+
+  @override
+  State<SearchMovieItem> createState() => _SearchMovieItemState();
+}
+
+class _SearchMovieItemState extends State<SearchMovieItem> {
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    return InkWell(
+      onTap: () {
+        Navigator.of(context)
+            .pushNamed(DetailsScreen.routeName, arguments: widget.movie);
+      },
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border(
+          bottom: BorderSide(color: MyTheme.greyColor2),
+        )),
+        width: double.infinity,
+        height: screenSize.height * .15,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: CachedNetworkImage(
+                  width: MediaQuery.of(context).size.width * .4,
+                  height: MediaQuery.of(context).size.height * .15,
+                  fit: BoxFit.fill,
+                  imageUrl:
+                      "${ApiConstants.baseImageUrl}${widget.movie?.posterPath}",
+                  placeholder: (context, url) => Center(
+                    child: CircularProgressIndicator(
+                      color: MyTheme.yellowColor,
+                    ),
+                  ),
+                  errorWidget: (context, url, error) => Icon(
+                    Icons.error,
+                    color: MyTheme.redColor,
+                  ),
+                ),
+              ),
+            ),
+            FutureBuilder<MovieDetailResponse>(
+                future: ApiManager.detailMoviesResponse(widget.movie?.id ?? 0),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child:
+                          CircularProgressIndicator(color: MyTheme.yellowColor),
+                    );
+                  } else if (snapshot.hasError ||
+                      snapshot.data?.success == "false") {
+                    return Center(
+                        child: IconButton(
+                      onPressed: () {
+                        ApiManager.detailMoviesResponse(widget.movie?.id ?? 0);
+                        setState(() {});
+                      },
+                      icon: const Icon(Icons.error),
+                      color: MyTheme.redColor,
+                    ));
+                  }
+                  var movieDetails = snapshot.data;
+
+                  return Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          movieDetails?.title ?? "",
+                          textAlign: TextAlign.left,
+                          softWrap: true,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleSmall
+                              ?.copyWith(
+                                  fontSize: 12, fontWeight: FontWeight.normal),
+                        ),
+                        Text(
+                          MyMethods.yearFromString(movieDetails?.releaseDate),
+                          textAlign: TextAlign.left,
+                          softWrap: true,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleSmall
+                              ?.copyWith(
+                                  fontSize: 11, fontWeight: FontWeight.normal),
+                        ),
+                        Text(
+                          movieDetails?.tagline ?? "",
+                          textAlign: TextAlign.left,
+                          softWrap: true,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleSmall
+                              ?.copyWith(
+                                  fontSize: 10, fontWeight: FontWeight.normal),
+                        ),
+                      ],
+                    ),
+                  );
+                })
+          ],
+        ),
+      ),
+    );
+  }
+}
